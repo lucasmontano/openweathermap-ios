1756d767fd7da621785de779ebae5735f7c3bb74
Fabio Akira <fab.takahashi@gmail.com>
2020-03-16 10:10:41 -0300

    Adding iOS base project

---
+++ openweathermap-ios/openweathermap-ios/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  openweathermap-ios
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
