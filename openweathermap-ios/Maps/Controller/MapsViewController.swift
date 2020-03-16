5c27229b3d250dd27b04234be2a5f052cf14ad3b
Felipe Weber <weberecomp@gmail.com>
2020-03-16 12:21:20 -0300

    Implemented the authorization to use GPS while using the App. If denied authorization, implemented the option to go in the settings to authorize the use of GPS.

--- openweathermap-ios/Maps/Controller/MapsViewController.swift
+++ openweathermap-ios/Maps/Controller/MapsViewController.swift
@@ -13,11 +13,33 @@ class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManager
     
 
     @IBOutlet weak var mapView: MKMapView!
+    var myLocationManager = CLLocationManager()
    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        myLocationManager.delegate = self
+        myLocationManager.desiredAccuracy = kCLLocationAccuracyBest
+        myLocationManager.requestWhenInUseAuthorization()
+        myLocationManager.startUpdatingLocation()
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status != .authorizedWhenInUse{
+            let alertController = UIAlertController(title: "Permission needed", message: "Without permission the app will not work correctly", preferredStyle: .alert)
+            let actionConfig = UIAlertAction(title: "Settings", style: .default) { (alertConfig) in
+                if let config = URL(string: UIApplication.openSettingsURLString){
+                    UIApplication.shared.open(config)
+                }
+            }
+            
+            let actionCancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+
+            alertController.addAction(actionConfig)
+            alertController.addAction(actionCancel)
+            present(alertController, animated: true, completion: nil)
+        }
     }
     
 }
