4a3f05e23f3f549bb116feabdaefa1d4794d7183
Cássio Sousa <cassio-sousa@outlook.com>
2020-05-08 16:40:58 -0300

    feat: Bookmark UIView

---
+++ openweathermap-ios/openweathermap-ios/BookmarkLocation/View/BookmarkViewController.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+class BookmarkViewController: UIViewController {
+
+    @IBOutlet weak var mainView: DiagonalBorderView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+// MARK: Animation apresentation
+extension BookmarkViewController: UIViewControllerTransitioningDelegate {
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return SlideAnimator()
+    }
+}
